43e6d7caa60b22ee05f3b838a88a8855f8220741
abdullahalnoman003 <noman22622@gmail.com>
2026-05-07 10:51:07 +0600

    Problem 1 and solution

---
+++ solutions.ts
@@ -0,0 +1,18 @@
+// Problem 1
+function filterEvenNumbers (num : number[] ): number[] {
+    const len = num.length;
+    const evenArray:number[] = [];
+    for(let i =0; i<len; i++){
+        if(num[i] %2 ===0){
+            evenArray.push(num[i]);
+        }
+    }
+    return evenArray;
+}
+// Problem 2 
+   
+// Problem 3
+// Problem 4
+// Problem 5
+// Problem 6
+// Problem 7
